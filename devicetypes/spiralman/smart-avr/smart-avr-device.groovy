181513bc7fde4e9c2fb8c0fd010be70a7c4cd048
Thomas Stephens <spiralman@gmail.com>
2016-05-15 15:02:22 -0500

    Adding basic UPnP communication, based on example SSDP test app

---
+++ devicetypes/spiralman/smart-avr/smart-avr-device.groovy
@@ -0,0 +1,29 @@
+metadata {
+	definition (name: "Denon AVR", namespace: "spiralman", author: "Thomas Stephens") {
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+
+}
+
+def sync(ip, port) {
+	def existingIp = getDataValue("ip")
+	def existingPort = getDataValue("port")
+	if (ip && ip != existingIp) {
+		updateDataValue("ip", ip)
+	}
+	if (port && port != existingPort) {
+		updateDataValue("port", port)
+	}
+}
