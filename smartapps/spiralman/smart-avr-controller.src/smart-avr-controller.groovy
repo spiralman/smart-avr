ebe898ddc97816c2ad17a8ae4c290c345a635fd2
Thomas Stephens <spiralman@gmail.com>
2016-05-15 22:11:20 -0500

    Untabifying and adding debug statement

--- smartapps/spiralman/smart-avr-controller.src/smart-avr-controller.groovy
+++ smartapps/spiralman/smart-avr-controller.src/smart-avr-controller.groovy
@@ -1,16 +1,16 @@
 definition(
-		name: "Smart AVR Controller",
+    name: "Smart AVR Controller",
     namespace: "spiralman",
-		author: "Thomas Stephens",
-		description: "An application for controlling Denon AVR receivers",
-		category: "SmartThings Labs",
-		iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-		iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-		iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    author: "Thomas Stephens",
+    description: "An application for controlling Denon AVR receivers",
+    category: "SmartThings Labs",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
-	page(
+  page(
     name: "deviceDiscovery",
     title: "Smart AVR Device Setup",
     content: "deviceDiscovery")
@@ -21,160 +21,161 @@ def getSearchTarget() {
 }
 
 def deviceDiscovery() {
-	def options = [:]
-	def devices = getVerifiedDevices()
-	devices.each {
-		def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
-		def key = it.value.mac
-		options["${key}"] = value
-	}
+  def options = [:]
+  def devices = getVerifiedDevices()
+  devices.each {
+    log.debug "Device info ${it.value}"
+    def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
+    def key = it.value.mac
+    options["${key}"] = value
+  }
 
-	ssdpSubscribe()
+  ssdpSubscribe()
 
-	ssdpDiscover()
-	verifyDevices()
+  ssdpDiscover()
+  verifyDevices()
 
-	return dynamicPage(name: "deviceDiscovery",
+  return dynamicPage(name: "deviceDiscovery",
                      title: "Discovery Started!",
                      nextPage: "",
                      refreshInterval: 5,
                      install: true,
                      uninstall: true) {
-		section("Please wait while we discover your UPnP Device. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
-			input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
-		}
-	}
+    section("Please wait while we discover your UPnP Device. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
+      input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
+    }
+  }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
+  log.debug "Installed with settings: ${settings}"
 
-	initialize()
+  initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+  log.debug "Updated with settings: ${settings}"
 
-	unsubscribe()
-	initialize()
+  unsubscribe()
+  initialize()
 }
 
 def initialize() {
-	unsubscribe()
-	unschedule()
+  unsubscribe()
+  unschedule()
 
-	ssdpSubscribe()
+  ssdpSubscribe()
 
-	if (selectedDevices) {
-		addDevices()
-	}
+  if (selectedDevices) {
+    addDevices()
+  }
 
-	runEvery5Minutes("ssdpDiscover")
+  runEvery5Minutes("ssdpDiscover")
 }
 
 void ssdpDiscover() {
-	sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${searchTarget}", physicalgraph.device.Protocol.LAN))
+  sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${searchTarget}", physicalgraph.device.Protocol.LAN))
 }
 
 void ssdpSubscribe() {
-	subscribe(location, "ssdpTerm.${searchTarget}", ssdpHandler)
+  subscribe(location, "ssdpTerm.${searchTarget}", ssdpHandler)
 }
 
 Map verifiedDevices() {
-	def devices = getVerifiedDevices()
-	def map = [:]
-	devices.each {
-		def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
-		def key = it.value.mac
-		map["${key}"] = value
-	}
-	map
+  def devices = getVerifiedDevices()
+  def map = [:]
+  devices.each {
+    def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
+    def key = it.value.mac
+    map["${key}"] = value
+  }
+  map
 }
 
 void verifyDevices() {
-	def devices = getDevices().findAll { it?.value?.verified != true }
-	devices.each {
-		int port = convertHexToInt(it.value.deviceAddress)
-		String ip = convertHexToIP(it.value.networkAddress)
-		String host = "${ip}:${port}"
-		sendHubCommand(new physicalgraph.device.HubAction("""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
-	}
+  def devices = getDevices().findAll { it?.value?.verified != true }
+  devices.each {
+    int port = convertHexToInt(it.value.deviceAddress)
+    String ip = convertHexToIP(it.value.networkAddress)
+    String host = "${ip}:${port}"
+    sendHubCommand(new physicalgraph.device.HubAction("""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
+  }
 }
 
 def getVerifiedDevices() {
-	getDevices().findAll{ it.value.verified == true }
+  getDevices().findAll{ it.value.verified == true }
 }
 
 def getDevices() {
-	if (!state.devices) {
-		state.devices = [:]
-	}
-	state.devices
+  if (!state.devices) {
+    state.devices = [:]
+  }
+  state.devices
 }
 
 def addDevices() {
-	def devices = getDevices()
+  def devices = getDevices()
 
-	selectedDevices.each { dni ->
-		def selectedDevice = devices.find { it.value.mac == dni }
-		def d
-		if (selectedDevice) {
-			d = getChildDevices()?.find {
-				it.deviceNetworkId == selectedDevice.value.mac
-			}
-		}
+  selectedDevices.each { dni ->
+    def selectedDevice = devices.find { it.value.mac == dni }
+    def d
+    if (selectedDevice) {
+      d = getChildDevices()?.find {
+        it.deviceNetworkId == selectedDevice.value.mac
+      }
+    }
 
-		if (!d) {
-			log.debug "Creating Generic UPnP Device with dni: ${selectedDevice.value.mac}"
-			addChildDevice("smartthings", "Generic UPnP Device", selectedDevice.value.mac, selectedDevice?.value.hub, [
-				"label": selectedDevice?.value?.name ?: "Generic UPnP Device",
-				"data": [
-					"mac": selectedDevice.value.mac,
-					"ip": selectedDevice.value.networkAddress,
-					"port": selectedDevice.value.deviceAddress
-				]
-			])
-		}
-	}
+    if (!d) {
+      log.debug "Creating Generic UPnP Device with dni: ${selectedDevice.value.mac}"
+      addChildDevice("smartthings", "Generic UPnP Device", selectedDevice.value.mac, selectedDevice?.value.hub, [
+        "label": selectedDevice?.value?.name ?: "Generic UPnP Device",
+        "data": [
+          "mac": selectedDevice.value.mac,
+          "ip": selectedDevice.value.networkAddress,
+          "port": selectedDevice.value.deviceAddress
+        ]
+      ])
+    }
+  }
 }
 
 def ssdpHandler(evt) {
-	def description = evt.description
-	def hub = evt?.hubId
+  def description = evt.description
+  def hub = evt?.hubId
 
-	def parsedEvent = parseLanMessage(description)
-	parsedEvent << ["hub":hub]
+  def parsedEvent = parseLanMessage(description)
+  parsedEvent << ["hub":hub]
 
-	def devices = getDevices()
-	String ssdpUSN = parsedEvent.ssdpUSN.toString()
-	if (devices."${ssdpUSN}") {
-		def d = devices."${ssdpUSN}"
-		if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
-			d.networkAddress = parsedEvent.networkAddress
-			d.deviceAddress = parsedEvent.deviceAddress
-			def child = getChildDevice(parsedEvent.mac)
-			if (child) {
-				child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
-			}
-		}
-	} else {
-		devices << ["${ssdpUSN}": parsedEvent]
-	}
+  def devices = getDevices()
+  String ssdpUSN = parsedEvent.ssdpUSN.toString()
+  if (devices."${ssdpUSN}") {
+    def d = devices."${ssdpUSN}"
+    if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
+      d.networkAddress = parsedEvent.networkAddress
+      d.deviceAddress = parsedEvent.deviceAddress
+      def child = getChildDevice(parsedEvent.mac)
+      if (child) {
+        child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
+      }
+    }
+  } else {
+    devices << ["${ssdpUSN}": parsedEvent]
+  }
 }
 
 void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
-	def body = hubResponse.xml
-	def devices = getDevices()
-	def device = devices.find { it?.key?.contains(body?.device?.UDN?.text()) }
-	if (device) {
-		device.value << [name: body?.device?.roomName?.text(), model:body?.device?.modelName?.text(), serialNumber:body?.device?.serialNum?.text(), verified: true]
-	}
+  def body = hubResponse.xml
+  def devices = getDevices()
+  def device = devices.find { it?.key?.contains(body?.device?.UDN?.text()) }
+  if (device) {
+    device.value << [name: body?.device?.roomName?.text(), model:body?.device?.modelName?.text(), serialNumber:body?.device?.serialNum?.text(), verified: true]
+  }
 }
 
 private Integer convertHexToInt(hex) {
-	Integer.parseInt(hex,16)
+  Integer.parseInt(hex,16)
 }
 
 private String convertHexToIP(hex) {
-	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+  [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
 }
