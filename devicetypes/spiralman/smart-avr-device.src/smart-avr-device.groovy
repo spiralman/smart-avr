1a598e1401c7e1133be845df5b396b96cefd7ba7
Thomas Stephens <spiralman@gmail.com>
2016-05-21 15:06:13 -0500

    Continuing to add input source control

--- devicetypes/spiralman/smart-avr-device.src/smart-avr-device.groovy
+++ devicetypes/spiralman/smart-avr-device.src/smart-avr-device.groovy
@@ -21,6 +21,10 @@ metadata {
         attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: '#ffffff'
         attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: '#79b821'
       }
+
+      tileAttribute("device.currentActivity", key: "SECONDARY_CONTROL") {
+        attributeState("default", label: '${currentActivity}', unit: "")
+      }
     }
 
     standardTile("refresh", "device.switch", inactiveLabel: false,
